ec6da35e54cca35396383ffca7096a43acbfe1f5
Ezekiel <iamwebwiz@gmail.com>
2019-06-20 04:43:24 +0100

    Add Interest model and migration

--- app/Property.php
+++ app/Property.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function applicants()
+    {
+        return $this->belongsToMany(User::class, 'interests', 'property_id', 'user_id');
+    }
 }
