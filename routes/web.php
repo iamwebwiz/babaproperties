430b7d87ceedb1aaf1b5d3de793eddb2cec2dd07
Ezekiel <iamwebwiz@gmail.com>
2019-06-21 10:32:38 +0100

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,22 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
     Route::get('/dashboard', 'HomeController@index')->name('admin.dashboard');
+
+    // properties routes
+    Route::prefix('properties')->group(function () {
+        Route::get('/', 'PropertyController@index')->name('admin.properties');
+        Route::get('/{id}/show', 'PropertyController@show')->name('admin.properties.show');
+        Route::get('/{propertyId}/applicants/{userId}/decline', 'PropertyController@declineInterest')
+            ->name('admin.properties.interests.decline');
+        Route::get('/{id}/applicants/{userId}/approve', 'PropertyController@approveInterest')
+            ->name('admin.properties.interests.approve');
+        Route::delete('/{property}', 'PropertyController@destroy')->name('admin.properties.destroy');
+    });
+
+    // users routes
+    Route::prefix('users')->group(function () {
+        Route::get('/', 'UserController@index')->name('admin.users');
+    });
 });
 
 Route::get('/logout', 'HomeController@logout');
